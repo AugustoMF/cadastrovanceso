b576659e4b84ebf60bb347f6c349299898d8f801
AugustoMF <114231094+AugustoMF@users.noreply.github.com>
2022-10-23 17:46:28 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+const nomeDono = document.getElementById ('nomeDono').value
+const idadeDono = document.getElementById (idadeDono).value
+const vanTipo = document.getElementById ('vanTipo').value
+const nomeEmpresa = document.getElementById ('nomeEmpresa').value
+const nomeMotorista = document.getElementById ('motoristaNome').value
+const motoristaIdade = document.getElementById ('motoristaNasc').value
+const passageiros = document.getElementById ('passageirosQuant').value
